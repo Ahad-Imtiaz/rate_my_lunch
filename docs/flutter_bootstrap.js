78079137a1acbc3474369df0aa56b5aa07abd9fd
Ahad Imtiaz <zakki074@gmail.com>
2025-08-22 11:16:57 +0200

    new web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"41da00ac46bc38a97a63ed0635450271d71afd
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3441717364"
+    serviceWorkerVersion: "4087075554"
   }
 });
